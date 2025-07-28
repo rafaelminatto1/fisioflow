9132a72c53bb086f36b4ec4ddec42e44f91f79cd
rafaelminatto1 <rafael@sateg.com.br>
2025-07-28 15:43:50 -0300

    fix: disable problematic ESLint rules in .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -47,9 +47,9 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-console': 'off',
     'import/no-unresolved': 'error',
     'import/order': [
       'warn',
